07b88b7c0b04589d87c48fe3cc09934dc2ce12c9
Harris Osserman <harris.osserman@gmail.com>
2016-10-09 18:56:00 -0400

    generate polynomial features

--- week-3/logistic_regression.py
+++ week-3/logistic_regression.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy
 import math
 
-ALPHA = .0005
+ALPHA = .005
 
 coordinates = []
 with open('ex2data1.txt') as inputfile:
@@ -23,7 +23,7 @@ gradient_1 = []
 
 theta_0 = 0
 theta_1 = 0
-for iteration in range(0, 5000):
+for iteration in range(0, 1):
 	theta_0_derivative = 0.0
 	theta_1_derivative = 0.0
 	for index in range(len(coordinates)):
@@ -34,13 +34,11 @@ for iteration in range(0, 5000):
 	gradient_1.append(ALPHA * (1.0 / len(coordinates)) * theta_1_derivative )
 	theta_0 = theta_0 - ALPHA * (1.0 / len(coordinates)) * theta_0_derivative  
 	theta_1 = theta_1 - ALPHA * (1.0 / len(coordinates)) * theta_1_derivative
-	print "theta_0", theta_0, "theta_1", theta_1
+	# print "theta_0", theta_0, "theta_1", theta_1
 
 def cost_function(theta, x, y):
 	theta_transpose_times_x = theta[0] * x[0] + theta[1] * x[1]
 	return (-1.0) * y * math.log(sigmoid(theta_transpose_times_x)) - (1.0 - y) * math.log(sigmoid(1.0 - theta_transpose_times_x))
-
-
 
 print "admission probability (should be 0.776): ", sigmoid(theta_0 * 45.0 + theta_1 * 85.0)
 
@@ -52,11 +50,11 @@ print "admission probability (should be 0.776): ", sigmoid(theta_0 * 45.0 + thet
 # plt.scatter(range(0, 5000), gradient_1)
 # plt.show()
 
-# admitted_training_set = filter(lambda row:sigmoid(row[0] * theta_0 + row[1] * theta_1) > 0.5, coordinates)
-# not_admitted_training_set = filter(lambda row:sigmoid(row[0] * theta_0 + row[1] * theta_1) <= 0.5, coordinates)
+admitted_training_set = filter(lambda row:sigmoid(row[0] * theta_0 + row[1] * theta_1) > 0.5, coordinates)
+not_admitted_training_set = filter(lambda row:sigmoid(row[0] * theta_0 + row[1] * theta_1) <= 0.5, coordinates)
 
-admitted_training_set = filter(lambda row:row[2] == 1, coordinates)
-not_admitted_training_set = filter(lambda row:row[2] == 0, coordinates)
+# admitted_training_set = filter(lambda row:row[2] == 1, coordinates)
+# not_admitted_training_set = filter(lambda row:row[2] == 0, coordinates)
 
 plt.scatter([exam_1[0] for exam_1 in admitted_training_set], [exam_2[1] for exam_2 in admitted_training_set], marker="o")
 plt.scatter([exam_1[0] for exam_1 in not_admitted_training_set], [exam_2[1] for exam_2 in not_admitted_training_set], marker="x")
