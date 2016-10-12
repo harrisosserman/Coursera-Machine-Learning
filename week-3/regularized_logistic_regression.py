f1dddc273485706c1306cae0fdb911815216d1e9
Harris Osserman <harris.osserman@human.x.ai>
2016-10-12 00:23:29 -0400

    graph the decision boundary

--- week-3/regularized_logistic_regression.py
+++ week-3/regularized_logistic_regression.py
@@ -22,7 +22,7 @@ polynomial_features = poly.fit_transform(map(lambda row:[row[0], row[1]], coordi
 
 theta = numpy.ones(len(polynomial_features[0]))
 
-for iteration in range(0, 15000):
+for iteration in range(0, 1500):
 	theta_derivatives = numpy.zeros(len(polynomial_features[0]))
 	for index in range(len(coordinates)):
 		prediction_for_row = (sigmoid(numpy.dot(polynomial_features[index], theta)) - coordinates[index][2])
@@ -45,7 +45,29 @@ for index in range(len(coordinates)):
 		predict_no.append(coordinates[index])
 
 
+plt.figure(1)
 plt.scatter([exam_1[0] for exam_1 in predict_yes], [exam_2[1] for exam_2 in predict_yes], marker="o")
+plt.scatter([exam_1[0] for exam_1 in predict_no], [exam_2[1] for exam_2 in predict_no], marker="x")
+
+
+# find the place where theta * x == 0
+x1_data_points = numpy.linspace(-1, 1, 100)
+x2_data_points = numpy.linspace(-1, 1, 100)
+data_points = []
+for x1_index in range(0, 100):
+	for x2_index in range(0, 100):
+		data_points.append([x1_data_points[x1_index], x2_data_points[x2_index]])
+poly = PolynomialFeatures(degree=6)
+polynomial_features = poly.fit_transform(map(lambda row:[row[0], row[1]], data_points))
+predict_yes = []
+predict_no = []
+for index in range(len(polynomial_features)):
+	if (sigmoid(numpy.dot(theta, polynomial_features[index])) > 0.5):
+		predict_yes.append(data_points[index])
+	else:
+		predict_no.append(data_points[index])
+
+plt.figure(2)
 plt.scatter([exam_1[0] for exam_1 in predict_no], [exam_2[1] for exam_2 in predict_no], marker="x")
 plt.show()
 
