de8daa6df1ba58b8e907903bcbc3f52eddd0f7bd
Harris Osserman <harris.osserman@human.x.ai>
2016-10-15 18:05:01 -0400

    vectorize cost function

--- week-4/handwritten_number_classifier.py
+++ week-4/handwritten_number_classifier.py
@@ -7,18 +7,14 @@ print "=====all keys: ", mat.keys()
 
 def unregularized_cost_function(theta, x, y):
 	# theta is assumed to be a vertical vector
-	print 'X shape: ', numpy.matrix(x).shape
-	print 'theta shape: ', numpy.matrix(theta).shape
-
-
 	theta_x = numpy.matrix(x) * numpy.matrix(theta)
 	h = 1/(1 + numpy.exp(theta_x))
 	log_h = numpy.log(h)
 	log_1_minus_h = numpy.log(1 - h)
-	temp_output = -1 * numpy.matrix(y) * log_h - (1 - numpy.matrix(y)) * log_1_minus_h
-	print temp_output
-	# ouptut = -1 * numpy.matrix(y)
+	# TODO: look into why you need to do the dot product
+	summation = -1 * numpy.dot(numpy.transpose(y), log_h) - numpy.dot((1 - numpy.transpose(y)), log_1_minus_h)
+	return (1.0 / len(y)) * summation[0][0]
 
 
 test_theta = numpy.ones((len(mat['X'][0]), 1))
-unregularized_cost_function(test_theta, mat['X'], mat['y'])
+print 'output of func: ', unregularized_cost_function(test_theta, mat['X'], mat['y'])
