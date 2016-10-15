54fcbfc7c9bc5af760a2e4157c35668a085ccb14
Harris Osserman <harris.osserman@human.x.ai>
2016-10-15 18:18:16 -0400

    WIP on unregularized gradient func

--- week-4/handwritten_number_classifier.py
+++ week-4/handwritten_number_classifier.py
@@ -5,10 +5,13 @@ print "=====all keys: ", mat.keys()
 # each row of X is an image with 20 different pixels and each row of y matches up to X.  
 # when y[i] = 0, that means that the image is a 10, and 1-9 is 1-9...
 
-def unregularized_cost_function(theta, x, y):
-	# theta is assumed to be a vertical vector
+def calculate_h(theta, x):
 	theta_x = numpy.matrix(x) * numpy.matrix(theta)
 	h = 1/(1 + numpy.exp(theta_x))
+	return h
+
+def unregularized_cost_function(theta, x, y):
+	h = calculate_h(theta, x)
 	log_h = numpy.log(h)
 	log_1_minus_h = numpy.log(1 - h)
 	# TODO: look into why you need to do the dot product
@@ -16,5 +19,14 @@ def unregularized_cost_function(theta, x, y):
 	return (1.0 / len(y)) * summation[0][0]
 
 
+def unregularized_gradient(theta, x, y):
+	h = calculate_h(theta, x)
+	h_minus_y = h - numpy.matrix(y)
+	print 'transpose x: ', numpy.transpose(x).shape
+	print 'h minus y: ', h_minus_y.shape
+	return (1.0 / len(y)) * numpy.multiply(numpy.transpose(x), h_minus_y)
+
+
 test_theta = numpy.ones((len(mat['X'][0]), 1))
-print 'output of func: ', unregularized_cost_function(test_theta, mat['X'], mat['y'])
+print 'output of unregularized cost func: ', unregularized_cost_function(test_theta, mat['X'], mat['y'])
+print 'output of unregularized gradient: ', unregularized_gradient(test_theta, mat['X'], mat['y'])
