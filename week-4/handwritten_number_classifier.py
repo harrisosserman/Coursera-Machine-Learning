8ae74469f7c77b2f8739fc3aa0c292ad381b6c8a
Harris Osserman <harris.osserman@human.x.ai>
2016-10-19 21:25:24 -0400

    generate precision numbers for my classifier

--- week-4/handwritten_number_classifier.py
+++ week-4/handwritten_number_classifier.py
@@ -22,7 +22,6 @@ def unregularized_cost_function(theta, x, y):
 def regularized_cost_function(theta, x, y):
 	summation_of_unregularized_cost = unregularized_cost_function(theta, x, y)
 	regularization_term = (0.1 / (2.0 * len(y))) * numpy.sum(numpy.power(theta, 2))
-	print 'regularize cost func output', summation_of_unregularized_cost + regularization_term
 	return summation_of_unregularized_cost + regularization_term
 
 def unregularized_gradient(theta, x, y):
@@ -32,17 +31,14 @@ def unregularized_gradient(theta, x, y):
 
 def regularized_gradient(theta, x, y):
 	# use a lambda of 0.1
-	print 'regularized gradient output shape: ', numpy.matrix(unregularized_gradient(theta, x, y) + (0.1 / len(y)) * numpy.transpose(theta)).shape 
 	return unregularized_gradient(theta, x, y) + (0.1 / len(y)) * numpy.transpose(theta) 
 
 def regularized_gradient_helper(theta, *args):
-	print 'in gradient helper, sizes are: ', theta.shape, args[0].shape, args[1].shape
+	# this is the crazy code that I need to do to convert a 
 	output = numpy.ravel(regularized_gradient(theta, args[0], args[1]).sum(axis=0))
-	print "regularized gradient helper flattened: ", output.shape
 	return output
 
 def regularized_cost_helper(theta, *args):
-	print 'in cost helper, sizes are: ', theta.shape, args[0].shape, args[1].shape
 	return regularized_cost_function(theta, args[0], args[1])
 
 initial_theta = numpy.random.rand(1, len(mat['X'][0]))
@@ -50,12 +46,31 @@ initial_theta = numpy.random.rand(1, len(mat['X'][0]))
 # print 'output of unregularized gradient: ', unregularized_gradient(initial_theta, mat['X'], mat['y'])
 # print 'output of regularized gradient: ', regularized_gradient(initial_theta, mat['X'], mat['y'])
 
-
-def done_with_iteration(output):
-	print '===DONE WITH ITERATION: ', output
-
 # iteration maps to the digit that we are building a classifier for (1-10)
+trained_thetas = []
 for iteration in range(1, 10):
 	y_vector = numpy.transpose(map(lambda row:1 if ((row[0] % iteration == 0 or (row[0] == 0 and iteration == 10))) else 0, mat['y']))
-	response = scipy.optimize.fmin_cg(regularized_cost_helper, initial_theta, regularized_gradient_helper, args=(mat['X'], y_vector), full_output=True, callback=done_with_iteration)
-	print response
+	response = scipy.optimize.fmin_cg(regularized_cost_helper, initial_theta, regularized_gradient_helper, args=(mat['X'], y_vector))
+	trained_thetas.append(response)
+
+# use the training set to predict outcomes
+number_of_correct_predictions = 0
+number_of_total_predictions = 0
+for image_index in range(len(mat['y'])):
+	best_prediction = 0
+	digit_representation = -1
+	for classifier in range(len(trained_thetas)):
+		prediction = calculate_h(trained_thetas[classifier], mat['X'][image_index])
+		if prediction > best_prediction:
+			best_prediction = prediction
+			digit_representation = classifier + 1
+	number_of_total_predictions += 1
+	if digit_representation == mat['y'][image_index]:
+		number_of_correct_predictions += 1 
+
+print 'number of total predictions: ', number_of_total_predictions
+print  'number of correct predictions: ', number_of_correct_predictions
+print 'precision: ', (float(number_of_correct_predictions) / number_of_total_predictions)
+
+
+
