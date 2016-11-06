a9a4f7ff5394ce52f47f4b352743b7be0af3c85c
Harris Osserman <harris.osserman@gmail.com>
2016-11-06 14:58:12 -0500

    WIP on calculating gradient

--- week-6/ex5/linearRegCostFunction.m
+++ week-6/ex5/linearRegCostFunction.m
@@ -18,9 +18,17 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+disp("size of theta is")
+disp(size(theta))
+disp("size of X is")
+disp(size(X))
+disp("size of y is")
+disp(size(y))
+J = (sum((X * theta .- y).^2) + sum(theta.^2) .* lambda) * 1 / (2 * m)
 
-
-
+grad = (1 / m) * sum(X * theta .- y) .* sum(X, 1) - lambda / m .* theta'
+disp("size of gradient: ")
+disp(size(grad))
 
 
 
